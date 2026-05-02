01b44d66b1484b7f75afd5a2ed08519df426b917
metalDogFish <58714411+metalDogFish@users.noreply.github.com>
2026-05-02 17:08:37 -0400

    add cube

---
+++ ExampleGameProj/Assets/Spinner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//this.getComponent<
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
